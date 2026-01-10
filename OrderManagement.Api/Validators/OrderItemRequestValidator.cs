a22f5272c776077baffde960d9ed983c98ea37ce
伊藤 駿 <rendya2501@gmail.com>
2026-01-10 20:31:03 +0900

    コメント修正。 エラー文を一時コメントアウト。

--- OrderManagement.Api/Validators/OrderItemRequestValidator.cs
+++ OrderManagement.Api/Validators/OrderItemRequestValidator.cs
@@ -11,11 +11,11 @@ public class OrderItemRequestValidator : AbstractValidator<OrderItemRequest>
     public OrderItemRequestValidator()
     {
         RuleFor(x => x.ProductId)
-            .GreaterThan(0)
-            .WithMessage("Product ID must be greater than 0.");
+            .GreaterThan(0);
+        //.WithMessage("Product ID must be greater than 0.");
 
         RuleFor(x => x.Quantity)
-            .GreaterThan(0)
-            .WithMessage("Quantity must be greater than 0.");
+            .GreaterThan(0);
+            //.WithMessage("Quantity must be greater than 0.");
     }
 }
--- OrderManagement.Api/Validators/UpdateInventoryRequestValidator.cs
+++ OrderManagement.Api/Validators/UpdateInventoryRequestValidator.cs
@@ -12,16 +12,16 @@ public class UpdateInventoryRequestValidator : AbstractValidator<UpdateInventory
     {
         RuleFor(x => x.ProductName)
             .NotEmpty()
-            .WithMessage("Product name is required.")
-            .MaximumLength(100)
-            .WithMessage("Product name must not exceed 100 characters.");
+            //.WithMessage("Product name is required.")
+            .MaximumLength(100);
+        //.WithMessage("Product name must not exceed 100 characters.");
 
         RuleFor(x => x.Stock)
-            .GreaterThanOrEqualTo(0)
-            .WithMessage("Stock must be greater than or equal to 0.");
+            .GreaterThanOrEqualTo(0);
+        //.WithMessage("Stock must be greater than or equal to 0.");
 
         RuleFor(x => x.UnitPrice)
-            .GreaterThan(0)
-            .WithMessage("Unit price must be greater than 0.");
+            .GreaterThan(0);
+            //.WithMessage("Unit price must be greater than 0.");
     }
 }
